969f8c990443888732c12e5644884462ffe33ab2
lDalcon <damianleo1991@hotmail.com>
2022-10-03 21:28:04 -0500

    Validar filtros diferencia en cambio

--- src/app/pages/financiero/diferencia-cambio/diferencia-cambio.component.ts
+++ src/app/pages/financiero/diferencia-cambio/diferencia-cambio.component.ts
@@ -7,7 +7,6 @@ import { SessionService } from 'src/app/shared/services/session.service';
 import * as xlsx from 'xlsx';
 import * as FileSaver from 'file-saver';
 const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-const EXCEL_EXTENSION = '.xlsx';
 
 @Component({
   selector: 'app-diferencia-cambio',
@@ -29,12 +28,13 @@ export class DiferenciaCambioComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.data = {dc_consolidado: [], dc_credito: [], dc_forward: [], dc_resumen_credito: []};
+    this.data = { dc_consolidado: [], dc_credito: [], dc_forward: [], dc_resumen_credito: [] };
   }
 
   obtenerDiferenciaCambio() {
+    if (!this.validarFiltroReporte()) return;
     this.isLoading = true;
-    let fecha = new Date(this.fechaReporte.getFullYear(), this.fechaReporte.getMonth() +1, 0);
+    let fecha = new Date(this.fechaReporte.getFullYear(), this.fechaReporte.getMonth() + 1, 0);
     this.reporteService.obtenerDiferenciaCambio({ fecha, nick: this.sessionService.usuario.nick, nit: this.regional.nit })
       .then((res: any) => {
         this.messageService.add({ severity: 'success', detail: res.message });
@@ -47,15 +47,23 @@ export class DiferenciaCambioComponent implements OnInit {
       })
   }
 
+  validarFiltroReporte() {
+    let errors: string[] = [];
+    if (!this.fechaReporte) errors.push('La fecha es obligatoria');
+    if (!this.regional) errors.push('La regional es obligatoria');
+    if (errors.length > 0) this.messageService.add({ severity: 'warn', detail: `${errors.join('. ')}` })
+    return errors.length === 0 ? true : false;
+  }
+
   exportar() {
-    if(this.data.dc_consolidado.length === 0) return;
+    if (this.data.dc_consolidado.length === 0) return;
     const dc_consolidado = xlsx.utils.json_to_sheet(this.data.dc_consolidado);
     const dc_credito = xlsx.utils.json_to_sheet(this.data.dc_credito);
     const dc_resumen_credito = xlsx.utils.json_to_sheet(this.data.dc_resumen_credito);
     const dc_forward = xlsx.utils.json_to_sheet(this.data.dc_forward);
     const workbook = {
-      Sheets: {dc_consolidado, dc_credito, dc_resumen_credito,dc_forward},
-      SheetNames: ['dc_consolidado', 'dc_credito', 'dc_resumen_credito','dc_forward']
+      Sheets: { dc_consolidado, dc_credito, dc_resumen_credito, dc_forward },
+      SheetNames: ['dc_consolidado', 'dc_credito', 'dc_resumen_credito', 'dc_forward']
     };
     const excelBuffer = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' })
     this.saveAsExcel(excelBuffer)
